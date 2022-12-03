3ecbaae8b5a11e8b96b8d41dde85230edc2017a5
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2022-12-03 03:06:23 +0100

    day3 init

---
+++ Rust/aoc2022/src/day3.rs
@@ -0,0 +1,44 @@
+#[aoc(day3, part1)]
+pub fn part_1(input: &str) -> u32 {
+    todo!()
+}
+
+#[aoc(day3, part2)]
+pub fn part_2(input: &str) -> u32 {
+    todo!()
+}
+
+mod test {
+    #![allow(unreachable_code, unused_imports)]
+    use super::*;
+
+    #[allow(dead_code)]
+    fn get_example_input() -> &'static str {
+        ""
+    }
+
+    #[allow(dead_code)]
+    fn get_real_input() -> &'static str {
+        include_str!("../input/2022/day3.txt")
+    }
+
+    #[test]
+    fn test_example_part1() {
+        assert_eq!(part_1(&get_example_input()), todo!());
+    }
+
+    #[test]
+    fn test_example_part2_() {
+        assert_eq!(part_2(&get_example_input()), todo!());
+    }
+
+    #[test]
+    fn test_real_part1() {
+        assert_eq!(part_1(&get_real_input()), todo!());
+    }
+
+    #[test]
+    fn test_real_part2() {
+        assert_eq!(part_2(&get_real_input()), todo!());
+    }
+}
