ffab0a431c66235db4557e58bd1a7db87a672624
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2022-12-05 21:58:57 +0100

    day5 little bit better

--- Rust/aoc2022/src/day5.rs
+++ Rust/aoc2022/src/day5.rs
@@ -1,69 +1,63 @@
-fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>>
-where
-    T: Clone,
-{
-    (0..v[0].len())
-        .map(|i| v.iter().map(|inner| inner[i].clone()).collect::<Vec<T>>())
-        .collect()
+fn generate_stacks(input: &str) -> Vec<Vec<u8>> {
+    let mut lines = input.lines().rev();
+    let stacks_num = lines
+        .next()
+        .unwrap()
+        .bytes()
+        .enumerate()
+        .filter(|(j, _)| j % 4 == 1)
+        .count();
+
+    let mut stacks = vec![vec![]; stacks_num];
+    for (i, (_, c)) in lines
+        .flat_map(|line| {
+            line.bytes()
+                .enumerate()
+                .filter(|(i, c)| i % 4 == 1 && (c.is_ascii_uppercase() || *c == b' '))
+        })
+        .enumerate()
+    {
+        if c != b' ' {
+            stacks[i % stacks_num].push(c);
+        }
+    }
+    stacks
 }
 
-// #[aoc_generator(day5)]
-// pub fn gen(input: &str) -> (Vec<Vec<Option<_>>>, Vec<[u8; 3]>) {
+fn generate_instruction(line: &str) -> [u8; 3] {
+    let mut line_bytes = line.bytes().skip(5);
+    [
+        line_bytes
+            .by_ref()
+            .take_while(|c| c.is_ascii_digit())
+            .fold(0, |acc, d| acc * 10 + (d & 0b00001111)),
+        line_bytes
+            .by_ref()
+            .skip(5)
+            .take_while(|c| c.is_ascii_digit())
+            .fold(0, |acc, d| acc * 10 + (d & 0b00001111)),
+        line_bytes
+            .skip(3)
+            .take_while(|c| c.is_ascii_digit())
+            .fold(0, |acc, d| acc * 10 + (d & 0b00001111)),
+    ]
+}
 
-// }
+fn get_top_of_stacks(stacks: &[Vec<u8>]) -> String {
+    let mut result = "".to_string();
+    for row in stacks {
+        if let Some(l) = row.last() {
+            result.push(*l as char);
+        }
+    }
+    result
+}
 
 #[aoc(day5, part1)]
 pub fn part_1(input: &str) -> String {
-    let (stacks_str, instructions_str) = input.split_once("\n\n").unwrap();
-
-    let mut stacks_tmp: Vec<Vec<Option<_>>> = vec![];
-    let mut instructions: Vec<[u8; 3]> = vec![];
-
-    for stack_str in stacks_str.lines() {
-        let mut lvl = vec![];
-        for (_, c) in stack_str.chars().enumerate().filter(|(j, _)| j % 4 == 1) {
-            if c == ' ' {
-                lvl.push(None);
-            } else {
-                lvl.push(Some(c))
-            }
-        }
-        stacks_tmp.push(lvl);
-    }
-    stacks_tmp.pop();
-
-    let mut stacks_tmp = transpose(stacks_tmp);
-    let mut stacks = vec![];
-
-    for row in &mut stacks_tmp {
-        row.reverse();
-        let mut r = vec![];
-        for c in row.iter().filter(|c| c.is_some()) {
-            r.push(c.unwrap());
-        }
-        stacks.push(r);
-    }
-
-    for line in instructions_str.lines() {
-        let mut line_bytes = line.bytes().skip(5);
-        instructions.push([
-            line_bytes
-                .by_ref()
-                .take_while(|c| c.is_ascii_digit())
-                .fold(0, |acc, d| acc * 10 + (d & 0b00001111)),
-            line_bytes
-                .by_ref()
-                .skip(5)
-                .take_while(|c| c.is_ascii_digit())
-                .fold(0, |acc, d| acc * 10 + (d & 0b00001111)),
-            line_bytes
-                .skip(3)
-                .take_while(|c| c.is_ascii_digit())
-                .fold(0, |acc, d| acc * 10 + (d & 0b00001111)),
-        ]);
-    }
-
-    for [amount, from, to] in instructions {
+    let (stack_lines, instruction_lines) = input.split_once("\n\n").unwrap();
+    let mut stacks = generate_stacks(stack_lines);
+    for [amount, from, to] in instruction_lines.lines().map(generate_instruction) {
         let mut pops = vec![];
         for _ in 0..amount {
             pops.push(stacks[(from - 1) as usize].pop().unwrap());
@@ -71,67 +65,15 @@ pub fn part_1(input: &str) -> String {
         stacks[(to - 1) as usize].extend(pops);
     }
 
-    let mut result = "".to_string();
-    for row in stacks {
-        if let Some(l) = row.last() {
-            result.push(*l)
-        }
-    }
-    result
+    get_top_of_stacks(&stacks)
 }
 
 #[aoc(day5, part2)]
 pub fn part_2(input: &str) -> String {
-    let (stacks_str, instructions_str) = input.split_once("\n\n").unwrap();
+    let (stack_lines, instruction_lines) = input.split_once("\n\n").unwrap();
+    let mut stacks = generate_stacks(stack_lines);
 
-    let mut stacks_tmp: Vec<Vec<Option<_>>> = vec![];
-    let mut instructions: Vec<[u8; 3]> = vec![];
-
-    for stack_str in stacks_str.lines() {
-        let mut lvl = vec![];
-        for (_, c) in stack_str.chars().enumerate().filter(|(j, _)| j % 4 == 1) {
-            if c == ' ' {
-                lvl.push(None);
-            } else {
-                lvl.push(Some(c))
-            }
-        }
-        stacks_tmp.push(lvl);
-    }
-    stacks_tmp.pop();
-
-    let mut stacks_tmp = transpose(stacks_tmp);
-    let mut stacks = vec![];
-
-    for row in &mut stacks_tmp {
-        row.reverse();
-        let mut r = vec![];
-        for c in row.iter().filter(|c| c.is_some()) {
-            r.push(c.unwrap());
-        }
-        stacks.push(r);
-    }
-
-    for line in instructions_str.lines() {
-        let mut line_bytes = line.bytes().skip(5);
-        instructions.push([
-            line_bytes
-                .by_ref()
-                .take_while(|c| c.is_ascii_digit())
-                .fold(0, |acc, d| acc * 10 + (d & 0b00001111)),
-            line_bytes
-                .by_ref()
-                .skip(5)
-                .take_while(|c| c.is_ascii_digit())
-                .fold(0, |acc, d| acc * 10 + (d & 0b00001111)),
-            line_bytes
-                .skip(3)
-                .take_while(|c| c.is_ascii_digit())
-                .fold(0, |acc, d| acc * 10 + (d & 0b00001111)),
-        ]);
-    }
-
-    for [amount, from, to] in instructions {
+    for [amount, from, to] in instruction_lines.lines().map(generate_instruction) {
         let mut pops = vec![];
         for _ in 0..amount {
             pops.push(stacks[(from - 1) as usize].pop().unwrap());
@@ -140,13 +82,7 @@ pub fn part_2(input: &str) -> String {
         stacks[(to - 1) as usize].extend(pops);
     }
 
-    let mut result = "".to_string();
-    for row in stacks {
-        if let Some(l) = row.last() {
-            result.push(*l)
-        }
-    }
-    result
+    get_top_of_stacks(&stacks)
 }
 
 mod test {
@@ -177,22 +113,22 @@ move 1 from 1 to 2
     }
 
     #[test]
-    fn test_example_part1() {
+    fn test_example_part1_2() {
         assert_eq!(part_1(get_example_input()).as_str(), "CMZ");
     }
 
     #[test]
-    fn test_example_part2_() {
+    fn test_example_part2_2() {
         assert_eq!(part_2(get_example_input()).as_str(), "MCD");
     }
 
     #[test]
-    fn test_real_part1() {
+    fn test_real_part1_2() {
         assert_eq!(part_1(get_real_input()).as_str(), "VJSFHWGFT");
     }
 
-    // #[test]
-    // fn test_real_part2() {
-    //     assert_eq!(part_2(get_real_input()).as_str(), "");
-    // }
+    #[test]
+    fn test_real_part2_2() {
+        assert_eq!(part_2(get_real_input()).as_str(), "LCTQFBVZV");
+    }
 }
