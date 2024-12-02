14a279ca9ecff15ec415ee8e41cc738056876d69
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2024-12-02 19:24:37 +0100

    day 2 done

---
+++ Rust/aoc2024/src/day2.rs
@@ -0,0 +1,108 @@
+use arrayvec::ArrayVec;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use utils::parse::ParseOps;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[derive(Debug)]
+struct Report {
+    levels: ArrayVec<Level, 8>,
+}
+
+impl Report {
+    fn new(line: &[u8]) -> Self {
+        let mut v: ArrayVec<Level, 8> = ArrayVec::new_const();
+        for int in line.iter_unsigned::<u8>() {
+            v.push(Level(int));
+        }
+
+        Self { levels: v }
+    }
+
+    fn is_safe_part1(&self) -> bool {
+        self.levels
+            .windows(3)
+            .all(|x| x[0].is_safe_diff(&x[1], &x[2]))
+    }
+
+    fn is_safe_part2(&self) -> bool {
+        (0..self.levels.len()).any(|i| {
+            let mut tmp = self.levels.clone();
+            tmp.swap_remove(i);
+            tmp.windows(3).all(|x| x[0].is_safe_diff(&x[1], &x[2]))
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Level(u8);
+
+impl Level {
+    fn is_safe_diff(&self, other_1: &Level, other_2: &Level) -> bool {
+        (self.0.abs_diff(other_1.0) <= 3)
+            && (other_1.0.abs_diff(other_2.0) <= 3)
+            && ((self.0 < other_1.0 && other_1.0 < other_2.0)
+                || (self.0 > other_1.0 && other_1.0 > other_2.0))
+    }
+}
+
+type ParseOut = Vec<Report>;
+
+#[aoc_generator(day2)]
+fn parse(input: &str) -> ParseOut {
+    input
+        .as_bytes()
+        .split(|c| *c == b'\n')
+        .map(Report::new)
+        .collect()
+}
+
+#[aoc(day2, part1)]
+fn part1(input: &ParseOut) -> usize {
+    input.iter().filter(|x| x.is_safe_part1()).count()
+}
+
+#[aoc(day2, part2)]
+fn part2(input: &ParseOut) -> usize {
+    input.into_par_iter().filter(|x| x.is_safe_part2()).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn real_input() -> String {
+        std::fs::read_to_string("./input/2024/day2.txt")
+            .unwrap()
+            .trim()
+            .to_string()
+    }
+
+    fn example_input() -> String {
+        "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"
+            .to_string()
+    }
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(&parse(&example_input())), 2);
+    }
+    #[test]
+    fn part1_real() {
+        assert_eq!(part1(&parse(&real_input())), 220);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse(&example_input())), 4);
+    }
+    #[test]
+    fn part2_real() {
+        assert_eq!(part2(&parse(&real_input())), 296);
+    }
+}
