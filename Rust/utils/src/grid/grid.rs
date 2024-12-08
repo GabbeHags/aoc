bc78beccd04d04cc9d1a2fd17cc1941693e1aed8
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2024-12-08 20:32:25 +0100

    added plus and cross shape to grid

--- Rust/utils/src/grid/grid.rs
+++ Rust/utils/src/grid/grid.rs
@@ -1,3 +1,9 @@
+use std::{
+    fmt::Debug,
+    iter::{repeat, zip},
+    ops::Range,
+};
+
 use rayon::{
     iter::{IntoParallelIterator, ParallelIterator},
     slice::ParallelSlice,
@@ -24,7 +30,7 @@ impl<T: Clone> Grid<T> {
     }
 }
 
-impl<T> Grid<T> {
+impl<T: Debug> Grid<T> {
     fn map_row_indices<F, B>(
         &self,
         f: F,
@@ -70,27 +76,22 @@ impl<T> Grid<T> {
     pub fn iter_all_surroundings(
         &self,
         size: usize,
-    ) -> impl Iterator<Item = impl Iterator<Item = impl Iterator<Item = impl Iterator<Item = &T>>>>
-    {
+    ) -> impl Iterator<Item = GridSurrounding<'_, T>> {
         self.map_row_indices(move |row, col| self.get_surrounding(row, col, size))
+            .flatten()
     }
 
-    pub fn get_surrounding(
-        &self,
-        row: usize,
-        col: usize,
-        size: usize,
-    ) -> impl Iterator<Item = impl Iterator<Item = &T>> {
+    pub fn get_surrounding(&self, row: usize, col: usize, size: usize) -> GridSurrounding<'_, T> {
         debug_assert!(row < self.height);
         debug_assert!(col < self.width);
         let rows = row.saturating_sub(size)..self.height.min(row.saturating_add(size + 1));
         let cols = col.saturating_sub(size)..self.width.min(col.saturating_add(size + 1));
 
-        rows.map(move |r| cols.clone().map(move |c| self.get(r, c)))
+        GridSurrounding::new(self.height, self.width, row, col, rows, cols, &self.grid)
     }
 }
 
-impl<T: Sync> Grid<T> {
+impl<T: Sync + Debug> Grid<T> {
     pub fn par_rows(&self) -> impl ParallelIterator<Item = &[T]> {
         self.grid.par_chunks_exact(self.width)
     }
@@ -101,6 +102,197 @@ impl<T: Sync> Grid<T> {
     }
 }
 
+#[derive(Debug)]
+pub struct GridSurrounding<'a, T> {
+    _height: usize,
+    width: usize,
+    row: usize,
+    col: usize,
+    row_range: Range<usize>,
+    col_range: Range<usize>,
+    items: &'a [T],
+}
+
+impl<'a, T: Debug> GridSurrounding<'a, T> {
+    fn new(
+        height: usize,
+        width: usize,
+        row: usize,
+        col: usize,
+        row_range: Range<usize>,
+        col_range: Range<usize>,
+        items: &'a [T],
+    ) -> Self {
+        Self {
+            _height: height,
+            width,
+            row,
+            col,
+            row_range,
+            col_range,
+            items,
+        }
+    }
+    fn row_size(&self) -> usize {
+        self.row_range.len()
+    }
+    fn col_size(&self) -> usize {
+        self.row_range.len()
+    }
+    pub fn get_from_middle(&self, local_row: isize, local_col: isize) -> Option<&T> {
+        let row = self.row.checked_add_signed(local_row)?;
+        let col = self.col.checked_add_signed(local_col)?;
+        if !(self.row_range.contains(&row) && self.col_range.contains(&col)) {
+            return None;
+        }
+        Some(&self.items.chunks_exact(self.width).nth(row).unwrap()[col])
+    }
+    pub fn get_from_top_left(&self, local_row: usize, local_col: usize) -> Option<&T> {
+        let row = self.row_range.start + local_row;
+        let col = self.col_range.start + local_col;
+        if !(self.row_range.contains(&row) && self.col_range.contains(&col)) {
+            return None;
+        }
+        Some(&self.items.chunks_exact(self.width).nth(row).unwrap()[col])
+    }
+    pub fn get_square(&self) -> impl Iterator<Item = &[T]> {
+        //#####
+        //#####
+        //#####
+        //#####
+        //#####
+        self.items
+            .chunks_exact(self.width)
+            .skip(self.row_range.start)
+            .take(self.row_size())
+            .map(|row| &row[self.col_range.clone()])
+    }
+    pub fn get_plus(
+        &self,
+    ) -> PlusIter<
+        impl Iterator<Item = &T>,
+        impl Iterator<Item = &T>,
+        impl Iterator<Item = &T>,
+        impl Iterator<Item = &T>,
+        &T,
+    > {
+        //  #
+        //  #
+        //#####
+        //  #
+        //  #
+
+        let top_range = zip(
+            (0..self.row_size() / 2 + 1)
+                .map(|row| -(row as isize))
+                .rev(),
+            repeat(0),
+        );
+        let plus_iter_top = top_range.filter_map(|(row, col)| self.get_from_middle(row, col));
+
+        let left_range = zip(
+            repeat(0),
+            (0..self.col_size() / 2 + 1)
+                .map(|row| -(row as isize))
+                .rev(),
+        );
+        let plus_iter_left = left_range.filter_map(|(row, col)| self.get_from_middle(row, col));
+
+        let right_range = zip(
+            repeat(0),
+            (0..self.col_size() / 2 + 1).map(|row| (row as isize)),
+        );
+        let plus_iter_right = right_range.filter_map(|(row, col)| self.get_from_middle(row, col));
+
+        let bottom_range = zip(
+            (0..self.row_size() / 2 + 1).map(|row| (row as isize)),
+            repeat(0),
+        );
+        let plus_iter_bottom = bottom_range.filter_map(|(row, col)| self.get_from_middle(row, col));
+
+        PlusIter {
+            plus_iter_top,
+            plus_iter_left,
+            plus_iter_right,
+            plus_iter_bottom,
+        }
+    }
+    pub fn get_cross(
+        &self,
+    ) -> CrossIter<
+        impl Iterator<Item = &T>,
+        impl Iterator<Item = &T>,
+        impl Iterator<Item = &T>,
+        impl Iterator<Item = &T>,
+        &T,
+    > {
+        //#   #
+        // # #
+        //  #
+        // # #
+        //#   #
+        let top_left_to_middle_range = zip(0..self.row_size() / 2 + 1, 0..self.col_size() / 2 + 1)
+            .map(|(row, col)| (-(row as isize), -(col as isize)))
+            .rev();
+        dbg!(top_left_to_middle_range.clone().collect::<Vec<_>>());
+        let cross_iter_top_left_to_middle =
+            top_left_to_middle_range.filter_map(|(row, col)| self.get_from_middle(row, col));
+
+        let top_right_to_middle_range = zip(0..self.row_size() / 2 + 1, 0..self.col_size() / 2 + 1)
+            .map(|(row, col)| (-(row as isize), col as isize))
+            .rev();
+        let cross_iter_top_right_to_middle =
+            top_right_to_middle_range.filter_map(|(row, col)| self.get_from_middle(row, col));
+
+        let bottom_left_to_middle_range =
+            zip(0..self.row_size() / 2 + 1, 0..self.col_size() / 2 + 1)
+                .map(|(row, col)| (row as isize, -(col as isize)))
+                .rev();
+        let cross_iter_bottom_left_to_middle =
+            bottom_left_to_middle_range.filter_map(|(row, col)| self.get_from_middle(row, col));
+
+        let bottom_right_to_middle_range =
+            zip(0..self.row_size() / 2 + 1, 0..self.col_size() / 2 + 1)
+                .map(|(row, col)| (row as isize, col as isize))
+                .rev();
+        let cross_iter_bottom_right_to_middle =
+            bottom_right_to_middle_range.filter_map(|(row, col)| self.get_from_middle(row, col));
+
+        CrossIter {
+            cross_iter_top_left_to_middle,
+            cross_iter_top_right_to_middle,
+            cross_iter_bottom_left_to_middle,
+            cross_iter_bottom_right_to_middle,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct PlusIter<T, L, R, B, Inner>
+where
+    T: Iterator<Item = Inner>,
+    L: Iterator<Item = Inner>,
+    R: Iterator<Item = Inner>,
+    B: Iterator<Item = Inner>,
+{
+    pub plus_iter_top: T,
+    pub plus_iter_left: L,
+    pub plus_iter_right: R,
+    pub plus_iter_bottom: B,
+}
+#[derive(Debug)]
+pub struct CrossIter<T, L, R, B, Inner>
+where
+    T: Iterator<Item = Inner>,
+    L: Iterator<Item = Inner>,
+    R: Iterator<Item = Inner>,
+    B: Iterator<Item = Inner>,
+{
+    pub cross_iter_top_left_to_middle: T,
+    pub cross_iter_top_right_to_middle: L,
+    pub cross_iter_bottom_left_to_middle: R,
+    pub cross_iter_bottom_right_to_middle: B,
+}
 #[cfg(test)]
 mod tests_grid {
     use std::iter::zip;
@@ -177,10 +369,8 @@ mod tests_grid {
             [5, 50, 500, 5000],
         ];
         let grid: Grid<u16> = Grid::to_grid(input);
-        let a = grid
-            .get_surrounding(1, 1, 0)
-            .map(|x| x.cloned().collect::<Vec<_>>())
-            .collect::<Vec<_>>();
+        let binding = grid.get_surrounding(1, 1, 0);
+        let a = binding.get_square().collect::<Vec<_>>();
         assert_eq!(a, vec![vec![20]])
     }
     #[test]
@@ -193,10 +383,8 @@ mod tests_grid {
             [5, 50, 500, 5000],
         ];
         let grid: Grid<u16> = Grid::to_grid(input);
-        let a = grid
-            .get_surrounding(1, 1, 1)
-            .map(|x| x.cloned().collect::<Vec<_>>())
-            .collect::<Vec<_>>();
+        let binding = grid.get_surrounding(1, 1, 1);
+        let a = binding.get_square().collect::<Vec<_>>();
         assert_eq!(
             a,
             vec![vec![1, 10, 100], vec![2, 20, 200], vec![3, 30, 300]]
@@ -212,10 +400,8 @@ mod tests_grid {
             [5, 50, 500, 5000],
         ];
         let grid: Grid<u16> = Grid::to_grid(input);
-        let a = grid
-            .get_surrounding(0, 0, 1)
-            .map(|x| x.cloned().collect::<Vec<_>>())
-            .collect::<Vec<_>>();
+        let binding = grid.get_surrounding(0, 0, 1);
+        let a = binding.get_square().collect::<Vec<_>>();
         assert_eq!(a, vec![vec![1, 10], vec![2, 20]])
     }
     #[test]
@@ -228,10 +414,8 @@ mod tests_grid {
             [5, 50, 500, 5000],
         ];
         let grid: Grid<u16> = Grid::to_grid(input);
-        let a = grid
-            .get_surrounding(4, 3, 1)
-            .map(|x| x.cloned().collect::<Vec<_>>())
-            .collect::<Vec<_>>();
+        let binding = grid.get_surrounding(4, 3, 1);
+        let a = binding.get_square().collect::<Vec<_>>();
         assert_eq!(a, vec![vec![400, 4000], vec![500, 5000]])
     }
     #[test]
@@ -244,10 +428,8 @@ mod tests_grid {
             [5, 50, 500, 5000, 50000],
         ];
         let grid: Grid<u16> = Grid::to_grid(input);
-        let a = grid
-            .get_surrounding(2, 2, 2)
-            .map(|x| x.cloned().collect::<Vec<_>>())
-            .collect::<Vec<_>>();
+        let binding = grid.get_surrounding(2, 2, 2);
+        let a = binding.get_square().collect::<Vec<_>>();
         assert_eq!(
             a,
             vec![
@@ -256,6 +438,227 @@ mod tests_grid {
                 vec![3, 30, 300, 3000, 30000],
                 vec![4, 40, 400, 4000, 40000],
                 vec![5, 50, 500, 5000, 50000],
+            ]
+        )
+    }
+
+    #[test]
+    fn test_plus_size_0() {
+        let input = [
+            [1, 10, 100, 1000],
+            [2, 20, 200, 2000],
+            [3, 30, 300, 3000],
+            [4, 40, 400, 4000],
+            [5, 50, 500, 5000],
+        ];
+        let grid: Grid<u16> = Grid::to_grid(input);
+        let binding = grid.get_surrounding(0, 0, 0);
+        dbg!(&binding);
+        let a = binding.get_plus();
+        assert_eq!(
+            [
+                a.plus_iter_top.cloned().collect::<Vec<_>>(),
+                a.plus_iter_left.cloned().collect::<Vec<_>>(),
+                a.plus_iter_right.cloned().collect::<Vec<_>>(),
+                a.plus_iter_bottom.cloned().collect::<Vec<_>>(),
+            ],
+            [vec![1], vec![1], vec![1], vec![1]]
+        )
+    }
+
+    #[test]
+    fn test_plus_size_1_left_edge() {
+        let input = [
+            [1, 10, 100, 1000],
+            [2, 20, 200, 2000],
+            [3, 30, 300, 3000],
+            [4, 40, 400, 4000],
+            [5, 50, 500, 5000],
+        ];
+        let grid: Grid<u16> = Grid::to_grid(input);
+        let binding = grid.get_surrounding(0, 0, 1);
+        let a = binding.get_plus();
+        assert_eq!(
+            [
+                a.plus_iter_top.cloned().collect::<Vec<_>>(),
+                a.plus_iter_left.cloned().collect::<Vec<_>>(),
+                a.plus_iter_right.cloned().collect::<Vec<_>>(),
+                a.plus_iter_bottom.cloned().collect::<Vec<_>>(),
+            ],
+            [vec![1], vec![1], vec![1, 10], vec![1, 2]]
+        )
+    }
+    #[test]
+    fn test_plus_size_1_right_edge() {
+        let input = [
+            [1, 10, 100, 1000],
+            [2, 20, 200, 2000],
+            [3, 30, 300, 3000],
+            [4, 40, 400, 4000],
+            [5, 50, 500, 5000],
+        ];
+        let grid: Grid<u16> = Grid::to_grid(input);
+        let binding = grid.get_surrounding(4, 3, 1);
+        dbg!(&binding);
+        let a = binding.get_plus();
+        assert_eq!(
+            [
+                dbg!(a.plus_iter_top.cloned().collect::<Vec<_>>()),
+                dbg!(a.plus_iter_left.cloned().collect::<Vec<_>>()),
+                dbg!(a.plus_iter_right.cloned().collect::<Vec<_>>()),
+                dbg!(a.plus_iter_bottom.cloned().collect::<Vec<_>>()),
+            ],
+            [vec![4000, 5000], vec![500, 5000], vec![5000], vec![5000]]
+        )
+    }
+
+    #[test]
+    fn test_plus_size_2() {
+        let input = [
+            [1, 10, 100, 1000, 10000],
+            [2, 20, 200, 2000, 20000],
+            [3, 30, 300, 3000, 30000],
+            [4, 40, 400, 4000, 40000],
+            [5, 50, 500, 5000, 50000],
+        ];
+        let grid: Grid<u16> = Grid::to_grid(input);
+        let binding = grid.get_surrounding(2, 2, 2);
+        let a = binding.get_plus();
+        assert_eq!(
+            [
+                a.plus_iter_top.cloned().collect::<Vec<_>>(),
+                a.plus_iter_left.cloned().collect::<Vec<_>>(),
+                a.plus_iter_right.cloned().collect::<Vec<_>>(),
+                a.plus_iter_bottom.cloned().collect::<Vec<_>>(),
+            ],
+            [
+                vec![100, 200, 300],
+                vec![3, 30, 300],
+                vec![300, 3000, 30000],
+                vec![300, 400, 500]
+            ]
+        )
+    }
+
+    #[test]
+    fn test_cross_size_0() {
+        let input = [
+            [1, 10, 100, 1000],
+            [2, 20, 200, 2000],
+            [3, 30, 300, 3000],
+            [4, 40, 400, 4000],
+            [5, 50, 500, 5000],
+        ];
+        let grid: Grid<u16> = Grid::to_grid(input);
+        let binding = grid.get_surrounding(0, 0, 0);
+        dbg!(&binding);
+        let a = binding.get_cross();
+        assert_eq!(
+            [
+                a.cross_iter_top_left_to_middle.cloned().collect::<Vec<_>>(),
+                a.cross_iter_top_right_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+                a.cross_iter_bottom_left_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+                a.cross_iter_bottom_right_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+            ],
+            [vec![1], vec![1], vec![1], vec![1]]
+        )
+    }
+
+    #[test]
+    fn test_cross_size_1_left_edge() {
+        let input = [
+            [1, 10, 100, 1000],
+            [2, 20, 200, 2000],
+            [3, 30, 300, 3000],
+            [4, 40, 400, 4000],
+            [5, 50, 500, 5000],
+        ];
+        let grid: Grid<u16> = Grid::to_grid(input);
+        let binding = grid.get_surrounding(0, 0, 1);
+        let a = binding.get_cross();
+        assert_eq!(
+            [
+                a.cross_iter_top_left_to_middle.cloned().collect::<Vec<_>>(),
+                a.cross_iter_top_right_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+                a.cross_iter_bottom_left_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+                a.cross_iter_bottom_right_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+            ],
+            [vec![1], vec![1], vec![1], vec![20, 1]]
+        )
+    }
+    #[test]
+    fn test_cross_size_1_right_edge() {
+        let input = [
+            [1, 10, 100, 1000],
+            [2, 20, 200, 2000],
+            [3, 30, 300, 3000],
+            [4, 40, 400, 4000],
+            [5, 50, 500, 5000],
+        ];
+        let grid: Grid<u16> = Grid::to_grid(input);
+        let binding = grid.get_surrounding(4, 3, 1);
+        dbg!(&binding);
+        let a = binding.get_cross();
+        assert_eq!(
+            [
+                a.cross_iter_top_left_to_middle.cloned().collect::<Vec<_>>(),
+                a.cross_iter_top_right_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+                a.cross_iter_bottom_left_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+                a.cross_iter_bottom_right_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+            ],
+            [vec![400, 5000], vec![5000], vec![5000], vec![5000]]
+        )
+    }
+
+    #[test]
+    fn test_cross_size_2() {
+        let input = [
+            [1, 10, 100, 1000, 10000],
+            [2, 20, 200, 2000, 20000],
+            [3, 30, 300, 3000, 30000],
+            [4, 40, 400, 4000, 40000],
+            [5, 50, 500, 5000, 50000],
+        ];
+        let grid: Grid<u16> = Grid::to_grid(input);
+        let binding = grid.get_surrounding(2, 2, 2);
+        let a = binding.get_cross();
+
+        assert_eq!(
+            [
+                a.cross_iter_top_left_to_middle.cloned().collect::<Vec<_>>(),
+                a.cross_iter_top_right_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+                a.cross_iter_bottom_left_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+                a.cross_iter_bottom_right_to_middle
+                    .cloned()
+                    .collect::<Vec<_>>(),
+            ],
+            [
+                vec![1, 20, 300],
+                vec![10000, 2000, 300],
+                vec![5, 40, 300],
+                vec![50000, 4000, 300]
             ]
         )
     }
